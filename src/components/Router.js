0717c8f61a0109e4956748ed050a99dc445d59c8
윤성 <yunyul1209@gmail.com>
2022-06-02 17:42:51 +0900

    feat(*) : Add logout function
    로그아웃 기능 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,18 @@ import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navagation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <HashRouter>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" element={<Profile />} />
           </>
         ) : (
           <>
