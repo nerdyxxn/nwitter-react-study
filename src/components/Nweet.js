71ef5cd953f8cafcb0c1c8268d33e8e31363ffb9
윤성 <yunyul1209@gmail.com>
2022-06-07 11:53:44 +0900

    feat(Nweet) : Add edit function refinement logic
    수정 기능을 구체화 시키기 위한 코드 추가

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,28 +1,42 @@
 import React, { useState } from "react";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { dbService } from "fbase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
   const [newNweet, setNewNweet] = useState(nweetObj.text);
-  //const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+  // DB에서 내가 선택한 트윗을 찾아서 핸들링 하기 위한 리터럴
+  const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
 
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if (ok) {
-      await deleteDoc(doc(dbService, "nweets", `${nweetObj.id}`));
+      await deleteDoc(NweetTextRef);
     }
   };
+
   const toggleEditing = () => setEditing((prev) => !prev);
-  const onSubmit = (e) => {
-    e.preventDefault();
+
+  const quitEditing = () => {
+    setEditing(false);
+    setNewNweet(nweetObj.text);
   };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await updateDoc(NweetTextRef, {
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+
   const onChange = (e) => {
     const {
       target: { value },
     } = e;
     setNewNweet(value);
   };
+
   return (
     <div>
       {/* editing이 true이면, 즉 수정중이면 수정form 보여주고 아니라면 기존의 트윗 리스트 출력*/}
@@ -35,9 +49,10 @@ const Nweet = ({ nweetObj, isOwner }) => {
               value={newNweet}
               required
               onChange={onChange}
-            ></input>
+            />
+            <input type="submit" value="Update Nweet" />
           </form>
-          <button onClick={toggleEditing}>Cancel</button>
+          <button onClick={quitEditing}>Cancel</button>
         </>
       ) : (
         <>
